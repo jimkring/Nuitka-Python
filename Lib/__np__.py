cfcc495f5209000e155c3cf34e9291cbb46f3e14
Maxwell175 <admin@mdtech.us>
2021-12-12 19:24:36 -0500

    __np__: Add find_dep_root method. Split out separate auto_patch_MD_MT_file method.

--- Lib/__np__.py
+++ Lib/__np__.py
@@ -283,6 +283,10 @@ def run_build_tool_exe(tool_name, exe, *args):
     return run_with_output(find_build_tool_exe(tool_name, exe), *args)
 
 
+def find_dep_root(dep_name):
+    return os.path.join(DEPENDENCY_INSTALL_DIR, dep_name)
+
+
 def find_dep_include(dep_name):
     return os.path.join(DEPENDENCY_INSTALL_DIR, dep_name, 'include')
 
@@ -304,50 +308,53 @@ def is_file_binary(file_path):
     with open(file_path, 'rb') as f:
         return bool(f.read(1024).translate(None, textchars))
 
+def auto_patch_MD_MT_file(fpath):
+    try:
+        if fpath.endswith('CMakeLists.txt'):
+            with open(fpath, 'r') as f:
+                s = f.read()
+            s2 = s.replace("/MD", "/MT")
+            s2 = s2.replace("-MD", "-MT")
+            s2 = re.sub(r"cmake_minimum_required *\( *VERSION [0-9\.]+ *\)",
+                        """cmake_minimum_required(VERSION 3.15)
+            set(CMAKE_MSVC_RUNTIME_LIBRARY MultiThreaded)
+            foreach(flag_var
+                        CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+                        CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO
+                        CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
+                        CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
+                    if(${flag_var} MATCHES "/MD")
+                        string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+                    endif()
+                endforeach(flag_var)
+
+         """, s2, flags=re.IGNORECASE)
+            s2 = re.sub(r"cmake_policy\(VERSION [0-9\.]+\)", "", s2, flags=re.IGNORECASE)
+            if s != s2:
+                my_print("Fixed up file: %s" % fpath, style="blue")
+                with open(fpath, "w") as f:
+                    f.write(s2)
+        elif not is_file_binary(fpath):
+            with open(fpath, 'r') as f:
+                s = f.read()
+            s2 = s.replace("/MD", "/MT")
+            s2 = s2.replace("-MD", "-MT")
+            if s != s2:
+                my_print("Fixed up file: %s" % fpath, style="blue")
+                with open(fpath, "w") as f:
+                    f.write(s2)
+    except:
+        pass
+
 
 def auto_patch_MD_MT(folder):
     for dname, dirs, files in os.walk(folder):
         for fname in files:
-            try:
-                fpath = os.path.join(dname, fname)
-                if '.git' in fpath or '.svn' in fpath:
-                    continue
-
-                if fname.endswith('CMakeLists.txt'):
-                    with open(fpath, 'r') as f:
-                        s = f.read()
-                    s2 = s.replace("/MD", "/MT")
-                    s2 = s2.replace("-MD", "-MT")
-                    s2 = re.sub(r"cmake_minimum_required *\( *VERSION [0-9\.]+ *\)",
-                                """cmake_minimum_required(VERSION 3.15)
-                    set(CMAKE_MSVC_RUNTIME_LIBRARY MultiThreaded)
-                    foreach(flag_var
-                                CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
-                                CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO
-                                CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
-                                CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-                            if(${flag_var} MATCHES "/MD")
-                                string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
-                            endif()
-                        endforeach(flag_var)
-                        
-                 """, s2, flags=re.IGNORECASE)
-                    s2 = re.sub(r"cmake_policy\(VERSION [0-9\.]+\)", "", s2, flags=re.IGNORECASE)
-                    if s != s2:
-                        my_print("Fixed up file: %s" % fpath, style="blue")
-                        with open(fpath, "w") as f:
-                            f.write(s2)
-                elif not is_file_binary(fpath):
-                    with open(fpath, 'r') as f:
-                        s = f.read()
-                    s2 = s.replace("/MD", "/MT")
-                    s2 = s2.replace("-MD", "-MT")
-                    if s != s2:
-                        my_print("Fixed up file: %s" % fpath, style="blue")
-                        with open(fpath, "w") as f:
-                            f.write(s2)
-            except:
+            fpath = os.path.join(dname, fname)
+            if '.git' in fpath or '.svn' in fpath:
                 continue
+
+            auto_patch_MD_MT_file(fpath)
 
 
 def auto_patch_Cython_memcpy(folder):
