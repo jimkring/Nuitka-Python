7f00095a8a2a28711cb1ae1aa952e66907d1960b
Kay Hayen <kay.hayen@gmail.com>
2022-01-29 22:10:39 +0100

    Sync our tools with 3.9 branch

---
+++ Lib/__np__/common.py
@@ -0,0 +1,347 @@
+from __future__ import print_function
+
+import contextlib
+import glob
+import os
+import re
+import shutil
+import stat
+import subprocess
+import sys
+import sysconfig
+import tempfile
+from distutils.util import get_platform  # pylint: disable=import-error
+
+
+def getDependencyInstallDir():
+    import sysconfig
+
+    return os.path.join(sysconfig.get_config_var("prefix"), "dependency_libs")
+
+
+def getToolsInstallDir():
+    import sysconfig
+
+    return os.path.join(sysconfig.get_config_var("prefix"), "build_tools")
+
+
+def getEnableStyleCode(style):
+    if style == "pink":
+        style = "\033[95m"
+    elif style == "blue":
+        style = "\033[94m"
+    elif style == "green":
+        style = "\033[92m"
+    elif style == "yellow":
+        style = "\033[93m"
+    elif style == "red":
+        style = "\033[91m"
+    elif style == "bold":
+        style = "\033[1m"
+    elif style == "underline":
+        style = "\033[4m"
+    else:
+        style = None
+
+    return style
+
+
+_enabled_ansi = False
+
+
+def _enableAnsi():
+    # singleton, pylint: disable=global-statement
+    global _enabled_ansi
+    if not _enabled_ansi:
+
+        # Only necessary on Windows, as a side effect of this, ANSI colors get enabled
+        # for the terminal and never deactivated, so we are free to use them after
+        # this.
+        if os.name == "nt":
+            os.system("")
+
+        _enabled_ansi = True
+
+
+def getDisableStyleCode():
+    return "\033[0m"
+
+
+def my_print(*args, **kwargs):
+    """Make sure we flush after every print.
+
+    Not even the "-u" option does more than that and this is easy enough.
+
+    Use kwarg style=[option] to print in a style listed below
+    """
+
+    file_output = kwargs.get("file", sys.stdout)
+    is_atty = file_output.isatty()
+
+    if "style" in kwargs:
+        style = kwargs["style"]
+        del kwargs["style"]
+
+        if "end" in kwargs:
+            end = kwargs["end"]
+            del kwargs["end"]
+        else:
+            end = "\n"
+
+        if style is not None and is_atty:
+            enable_style = getEnableStyleCode(style)
+
+            if enable_style is None:
+                raise ValueError(
+                    "%r is an invalid value for keyword argument style" % style
+                )
+
+            _enableAnsi()
+
+            print(enable_style, end="", **kwargs)
+
+        print(*args, end=end, **kwargs)
+
+        if style is not None and is_atty:
+            print(getDisableStyleCode(), end="", **kwargs)
+    else:
+        print(*args, **kwargs)
+
+    # Flush the output.
+    file_output.flush()
+
+
+@contextlib.contextmanager
+def TemporaryDirectory():
+
+    dirpath = tempfile.mkdtemp()
+    yield dirpath
+
+    def delete_readonly_file(_, path, e):
+        if len(e) > 2 and e[1].errno == 13:
+            os.chmod(path, stat.S_IWRITE)
+            os.unlink(path)
+        else:
+            raise e[1]
+
+    shutil.rmtree(dirpath, onerror=delete_readonly_file)
+
+
+class NoSuchURL(Exception):
+    pass
+
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+
+def download_file(url, destination):
+    if str is bytes:
+        from urllib2 import HTTPError, Request, urlopen
+    else:
+        from urllib.request import HTTPError, Request, urlopen
+
+    try:
+        my_print("Attempting to download '%s'." % url, style="blue")
+
+        req = Request(url, headers={"User-Agent": "Nuitka-Python"})
+        with contextlib.closing(urlopen(req)) as fp:
+            if (
+                "content-disposition" in fp.headers
+                and "filename=" in fp.headers["content-disposition"]
+            ):
+                destination_file = os.path.join(
+                    destination,
+                    fp.headers["content-disposition"].split("filename=")[-1].strip('"'),
+                )
+            else:
+                destination_file = os.path.join(
+                    destination, os.path.basename(fp.geturl())
+                )
+
+            parent_dir = os.path.dirname(destination_file)
+            if not os.path.exists(parent_dir):
+                os.makedirs(parent_dir)
+
+            with open(destination_file, "wb") as out_file:
+                bs = 1024 * 8
+                while True:
+                    block = fp.read(bs)
+                    if not block:
+                        break
+                    out_file.write(block)
+
+    except HTTPError as e:
+        if e.code == 404:
+            raise NoSuchURL(url)
+        else:
+            raise
+
+    return destination_file
+
+
+def extract_archive(archive_file, destination=None):
+    if destination is None:
+        destination = os.path.splitext(archive_file)[0]
+        if destination.endswith(".tar"):
+            destination = destination[:-4]
+    shutil.unpack_archive(archive_file, destination)
+    return destination
+
+
+def download_extract(url, destination):
+    with TemporaryDirectory() as dir:
+        downloaded_file = download_file(url, dir)
+        extract_archive(downloaded_file, destination)
+
+
+def run_with_output(*args, **kwargs):
+    import subprocess
+
+    stdin = kwargs.pop("stdin", None)
+    assert not kwargs
+
+    p = subprocess.Popen(
+        args,
+        universal_newlines=True,
+        stdin=stdin,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+
+    output = ""
+    for line in p.stdout:
+        sys.stdout.write(line)
+        sys.stdout.flush()
+        output += line
+    p.wait()
+    if p.returncode != 0:
+        raise subprocess.CalledProcessError(p.returncode, args, output)
+    return output
+
+
+def install_files(dst, *files, base_dir=None):
+    if not os.path.isdir(dst):
+        os.makedirs(dst, exist_ok=True)
+    for file_glob in files:
+        for file in glob.glob(file_glob):
+            destination_filename = os.path.basename(file)
+            if base_dir is not None and file.startswith(base_dir):
+                destination_filename = file[len(base_dir) + 1 :]
+            file_dst = os.path.join(dst, destination_filename)
+            if not os.path.exists(os.path.dirname(file_dst)):
+                os.makedirs(os.path.dirname(file_dst))
+            if os.path.isdir(file):
+                copytree(file, os.path.join(dst, destination_filename))
+            else:
+                shutil.copy(file, os.path.join(dst, destination_filename))
+
+
+def install_dep_include(dependency_name, *files, base_dir=None):
+    dependency_location = os.path.join(
+        getDependencyInstallDir(), dependency_name, "include"
+    )
+    install_files(dependency_location, *files, base_dir=base_dir)
+
+
+def install_dep_libs(dependency_name, *files, base_dir=None):
+    dependency_location = os.path.join(
+        getDependencyInstallDir(), dependency_name, "libs"
+    )
+    install_files(dependency_location, *files, base_dir=base_dir)
+
+
+def install_build_tool(tool_name, *files, base_dir=None):
+    dependency_location = os.path.join(getToolsInstallDir(), tool_name)
+    install_files(dependency_location, *files, base_dir=base_dir)
+
+
+def find_build_tool_exe(tool_name, exe):
+    if os.name != "nt" and tool_name == "patch":
+        return "patch"
+
+    return (
+        glob.glob(os.path.join(BUILD_TOOLS_INSTALL_DIR, tool_name, exe))
+        + glob.glob(os.path.join(BUILD_TOOLS_INSTALL_DIR, tool_name, "bin", exe))
+    )[0]
+
+
+def run_build_tool_exe(tool_name, exe, *args, **kwargs):
+    return run_with_output(find_build_tool_exe(tool_name, exe), *args, **kwargs)
+
+
+def apply_patch(patch_file, directory):
+    """Apply a patch file to a directory."""
+    my_print("Applying patch '%s' to '%s'" % (patch_file, directory))
+    with open(patch_file, "rb") as stdin:
+        run_build_tool_exe(
+            "patch",
+            "patch.exe" if os.name == "nt" else "patch",
+            "-d",
+            directory,
+            "-p",
+            "1",
+            "--verbose",
+            stdin=stdin,
+        )
+
+
+def find_dep_root(dep_name):
+    return os.path.join(getDependencyInstallDir(), dep_name)
+
+
+def find_dep_include(dep_name):
+    return os.path.join(getDependencyInstallDir(), dep_name, "include")
+
+
+def find_dep_libs(dep_name):
+    return os.path.join(getDependencyInstallDir(), dep_name, "libs")
+
+
+def prepend_to_file(file, prepend_str):
+    output = prepend_str
+    with open(file, "r") as f:
+        output += f.read()
+    with open(file, "w") as f:
+        f.write(output)
+
+
+def is_file_binary(file_path):
+    textchars = bytearray({7, 8, 9, 10, 12, 13, 27} | set(range(0x20, 0x100)) - {0x7F})
+    with open(file_path, "rb") as f:
+        return bool(f.read(1024).translate(None, textchars))
+
+
+def shall_link_statically(name):
+    import fnmatch
+
+    static_pattern = os.environ.get("NUITKA_PYTHON_STATIC_PATTERN")
+    if not static_pattern or not fnmatch.fnmatch(name, static_pattern):
+        return False
+
+    return True
+
+
+def write_linker_json(
+    result_path, libraries, library_dirs, runtime_library_dirs, extra_args
+):
+    import json
+
+    with open(result_path + ".link.json", "w") as f:
+        json.dump(
+            {
+                "libraries": libraries,
+                "library_dirs": library_dirs,
+                "runtime_library_dirs": runtime_library_dirs,
+                "extra_postargs": extra_args,
+            },
+            f,
+        )
--- Lib/rebuildpython.py
+++ Lib/rebuildpython.py
@@ -1,17 +1,20 @@
 from __future__ import print_function
 
-import sys
-import os
-import shutil
-import subprocess
+import ctypes
 import distutils
 import distutils.ccompiler
-import sysconfig
-import json
-import tempfile
 import fnmatch
+import json
+import os
+import platform
+import shutil
+import subprocess
+import sys
+import sysconfig
+import tempfile
 
 MOVEFILE_DELAY_UNTIL_REBOOT = 4
+
 
 def find_files(directory, pattern):
     for root, dirs, files in os.walk(directory):
@@ -19,6 +22,34 @@ def find_files(directory, pattern):
             if fnmatch.fnmatch(basename, pattern):
                 filename = os.path.join(root, basename)
                 yield filename
+
+
+def getPythonInitFunctions(compiler, filename):
+    if platform.system() == "Windows":
+        initFunctions = [
+            x.decode("ascii")
+            for x in subprocess.check_output(
+                [compiler.dumpbin, "/linkermember", filename]
+            ).split(b"\r\n")
+            if (b"init" if str is bytes else b"PyInit_") in x
+        ]
+    else:
+        functions = [
+            x.decode("ascii").split(" ")[-1]
+            for x in subprocess.check_output(["nm", filename]).split(
+                os.linesep.encode("ascii")
+            )
+        ]
+        initFunctions = [
+            x for x in functions if x.startswith("init" if str is bytes else "PyInit_")
+        ]
+
+    initFunctions = [
+        y for y in initFunctions if "$" not in y and "@" not in y and "?" not in y
+    ]
+
+    return initFunctions
+
 
 def run_rebuild():
     installDir = os.path.dirname(sys.executable)
@@ -35,168 +66,307 @@ def run_rebuild():
     os.environ["CXX"] = cxx_config_var
 
     compiler = distutils.ccompiler.new_compiler(verbose=5)
-    #compiler.initialize()
+    compiler.set_executables(
+        compiler=cc_config_var,
+        compiler_so=cc_config_var,
+        linker_exe=cc_config_var,
+        compiler_cxx=cxx_config_var,
+    )
+
+    try:
+        compiler.initialize()
+    except AttributeError:
+        pass
 
     foundLibs = {}
     checkedLibs = set()
 
+    from distutils.sysconfig import get_config_var
+
+    ext_suffix = get_config_var("EXT_SUFFIX")
+
     # Scan sys.path for any more lingering static libs.
     for path in reversed(sys.path):
-        if path == installDir:
+        # Ignore the working directory so we don't grab duplicate stuff.
+        if path == installDir or path == os.getcwd():
             continue
-        for file in find_files(path, '*.a'):
+        for file in find_files(
+            path, "*.lib" if platform.system() == "Windows" else "*.a"
+        ):
             if file in checkedLibs:
                 continue
 
-            _, filename = os.path.split(file)
+            filename_base = os.path.basename(filename)
 
-            if not filename.startswith("lib") or file.endswith(sysconfig.get_config_var("LIBRARY")):
+            if not filename_base.startswith("lib") or filename.endswith(
+                sysconfig.get_config_var("LIBRARY")
+            ):
                 continue
-            checkedLibs.add(file)
-            functions = [x.decode('ascii').split(' ')[-1] for x in
-                             subprocess.check_output(['nm', file]).split(os.linesep.encode('ascii'))]
-            initFunctions = [x for x in functions if x.startswith('init')]
 
-            # If this lib has a PyInit function, we should link it in.
+            checkedLibs.add(filename)
+
+            initFunctions = getPythonInitFunctions(compiler, file)
+
+            # If this lib has a Python init function, we should link it in.
             if initFunctions:
                 relativePath = os.path.relpath(file, path)
-                if 'site-packages' in relativePath:
+                if "site-packages" in relativePath:
                     continue
-                relativePath = relativePath.replace('.a', '')
                 dirpath, filename = os.path.split(relativePath)
-                filename = filename[3:]
-                relativePath = dirpath.replace('\\', '.').replace('/', '.') + '.' + filename
+                if platform.system() != "Windows" and filename.startswith("lib"):
+                    filename = filename[3:]
+                if filename.endswith(ext_suffix):
+                    filename = filename[: len(ext_suffix) * -1]
+                if filename.endswith(".lib"):
+                    filename = filename[:-4]
+                if filename.endswith(".a"):
+                    filename = filename[:-2]
+                relativePath = (
+                    dirpath.replace("\\", ".").replace("/", ".") + "." + filename
+                )
                 print(relativePath, file)
                 foundLibs[relativePath] = file
 
-    print('Scanning for any additional libs to link...')
+    print("Scanning for any additional libs to link...")
 
     # Start with the libs needed for a base interpreter.
-    link_libs = ['m']
+    if platform.system() == "Windows":
+        link_libs = [
+            "advapi32",
+            "shell32",
+            "ole32",
+            "oleaut32",
+            "kernel32",
+            "user32",
+            "gdi32",
+            "winspool",
+            "comdlg32",
+            "uuid",
+            "odbc32",
+            "odbccp32",
+            "shlwapi",
+            "ws2_32",
+            "version",
+            "libssl",
+            "libcrypto",
+            "tcl86t",
+            "tk86t",
+            "Crypt32",
+            "Iphlpapi",
+            "msi",
+            "Rpcrt4",
+            "Cabinet",
+            "winmm",
+        ]
+    else:
+        link_libs = ["m"]
 
-    library_dirs = [sysconfig.get_config_var('prefix'), sysconfig.get_config_var('LIBDEST')]
+    if platform.system() == "Windows":
+        library_dirs = [
+            sysconfig.get_config_var("srcdir"),
+            os.path.join(sysconfig.get_config_var("srcdir"), "libs"),
+            os.path.join(sysconfig.get_config_var("srcdir"), "tcl"),
+        ]
+    else:
+        library_dirs = [
+            sysconfig.get_config_var("prefix"),
+            sysconfig.get_config_var("LIBDEST"),
+            sysconfig.get_config_var("LIBDIR"),
+        ]
 
     # Scrape all available libs from the libs directory. We will let the linker worry about filtering out extra symbols.
-    for file in find_files(sysconfig.get_config_var('LIBDEST'), '*.a'):
+    for file in find_files(
+        sysconfig.get_config_var("prefix"),
+        "*.lib" if platform.system() == "Windows" else "*.a",
+    ):
         link_libs.append(file)
 
     for _name, path in foundLibs.items():
         link_libs += [path]
 
-        if os.path.isfile(path + '.link.json'):
-            with open(path + '.link.json', 'r') as f:
+        if os.path.isfile(path + ".link.json"):
+            with open(path + ".link.json", "r") as f:
                 linkData = json.load(f)
                 print(linkData)
-                link_libs += linkData['libraries']
-                library_dirs += [os.path.join(os.path.dirname(path), x) for x in linkData['library_dirs']]
+                link_libs += linkData["libraries"]
+                library_dirs += [
+                    os.path.join(os.path.dirname(path), x)
+                    for x in linkData["library_dirs"]
+                ]
 
     link_libs = list(set(link_libs))
     library_dirs = list(set(library_dirs))
 
     print("Generating interpreter sources...")
 
-    staticinitheader = """/* Minimal main program -- everything is loaded from the library */
+    staticinitheader = """#ifndef Py_STATICINIT_H
+#define Py_STATICINIT_H
 
-#include "Python.h"
+#include "object.h"
+#include "import.h"
 
-#ifdef __FreeBSD__
-#include <fenv.h>
+#define NUITKA_PYTHON_STATIC
+
+#if defined(Py_BUILD_CORE) && !defined(Py_BUILD_CORE_MODULE)
+#ifdef __cplusplus
+extern "C" {
 #endif
-
 """
 
-    for key, value in foundLibs.items():
-        functions = [x.decode('ascii').split(' ')[-1] for x in
-                     subprocess.check_output(['nm', value]).split(os.linesep.encode('ascii'))]
-        initFunctions = [x for x in functions if x.startswith('init')]
+    inittab_code = ""
+
+    for module_fullname, filename in foundLibs.items():
+        initFunctions = getPythonInitFunctions(compiler, filename)
+
         if not initFunctions:
-            print("Init not found!", key, value)
+            print("Init not found!", module_fullname, filename)
             continue
-        if "init" + key.split(".")[-1] in initFunctions:
-            initFunction = "init" + key.split(".")[-1]
-        else:
-            initFunction = initFunctions[-1]
-        staticinitheader += "	extern  void " + initFunction + "(void);\n"
 
+        module_basename = module_fullname.split(".")[-1]
 
-    staticinitheader += """
+        module_initfunc_name = ("init" if str is bytes else "PyInit_") + module_basename
 
-int
-main(int argc, char **argv)
-{
-"""
+        # We might have packages that rename their build functioons.
+        if module_initfunc_name not in initFunctions:
+            module_initfunc_name = initFunctions[-1]
 
-    for key, value in foundLibs.items():
-        functions = [x.decode('ascii').split(' ')[-1] for x in
-                     subprocess.check_output(['nm', value]).split(os.linesep.encode('ascii'))]
-        initFunctions = [x for x in functions if x.startswith('init')]
-        if not initFunctions:
-            continue
-        if "init" + key.split(".")[-1] in initFunctions:
-            initFunction = "init" + key.split(".")[-1]
-        else:
-            initFunction = initFunctions[-1]
-        staticinitheader += "	PyImport_AppendInittab(\"" + key + "\", " + initFunction + ");\n"
+        staticinitheader += "   extern  PyObject* " + module_initfunc_name + "(void);\n"
+        inittab_code += (
+            '   PyImport_AppendInittab("'
+            + module_fullname
+            + '", '
+            + module_initfunc_name
+            + ");\n"
+        )
 
-    staticinitheader += """
-	/* 754 requires that FP exceptions run in "no stop" mode by default,
-	 * and until C vendors implement C99's ways to control FP exceptions,
-	 * Python requires non-stop mode.  Alas, some platforms enable FP
-	 * exceptions by default.  Here we disable them.
-	 */
-#ifdef __FreeBSD__
-	fedisableexcept(FE_OVERFLOW);
-#endif
-	return Py_Main(argc, argv);
+    staticinitheader += (
+        """
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+static inline void Py_InitStaticModules(void) {
+%s
 }
 
-"""
+#endif
 
-    with open(os.path.join(sysconfig.get_config_var('prefix'), 'python.c'), 'w') as f:
+#endif // !Py_STATICINIT_H
+"""
+        % inittab_code
+    )
+
+    with open(
+        os.path.join(sysconfig.get_config_var("INCLUDEPY"), "staticinit.h"), "w"
+    ) as f:
         f.write(staticinitheader)
 
-    print('Compiling new interpreter...')
+    print("Compiling new interpreter...")
 
-    build_dir = os.path.join(sysconfig.get_config_var('prefix'), 'interpreter_build')
+    if platform.system() == "Windows":
+        interpreter_prefix = sysconfig.get_config_var("srcdir")
+    else:
+        interpreter_prefix = sysconfig.get_config_var("prefix")
+
+    build_dir = os.path.join(interpreter_prefix, "interpreter_build")
 
     if os.path.isdir(build_dir):
         shutil.rmtree(build_dir)
 
-    include_dirs = [sysconfig.get_config_var('INCLUDEPY')]
-    macros = [('Py_BUILD_CORE', None)]
+    include_dirs = [sysconfig.get_config_var("INCLUDEPY")]
+    macros = [("Py_BUILD_CORE", None)]
 
-    os.chdir(sysconfig.get_config_var('prefix'))
+    os.chdir(interpreter_prefix)
 
-    compiler.compile(["python.c"], output_dir=build_dir, include_dirs=include_dirs, macros=macros)
+    if platform.system() == "Windows":
+        compiler.compile(
+            ["python.c"], output_dir=build_dir, include_dirs=include_dirs, macros=macros
+        )
 
-    compiler.link_executable(
-        objects = [os.path.join(build_dir, 'python.o')],
-        output_progname='python',
-        output_dir=build_dir,
-        libraries=link_libs,
-        library_dirs=library_dirs,
-        extra_postargs=["-lm", "-pthread", "-lutil", "-ldl"] + sysconfig.get_config_var("LDFLAGS").split() + sysconfig.get_config_var("CFLAGS").split()
-    )
+        compiler.link_executable(
+            [os.path.join(build_dir, "python.obj")],
+            "python",
+            output_dir=build_dir,
+            libraries=linkLibs,
+            library_dirs=library_dirs,
+            extra_preargs=["/LTCG", "/USEPROFILE:PGD=python.pgd"],
+        )
 
-    # Replace running interpreter by moving current version to a temp file, then deleting it. This
-    # is to avoid Windows locks
-    interpreter_path = sys.executable
-    tmp = tempfile.NamedTemporaryFile(delete=False, dir=os.path.dirname(sys.executable))
-    tmp.close()
-    os.unlink(tmp.name)
-    os.rename(sys.executable, tmp.name)
-    os.unlink(tmp.name)
+        # Replace running interpreter by moving current version to a temp file, then marking it for deletion.
+        interpreter_path = sys.executable
+        tmp = tempfile.NamedTemporaryFile(delete=False)
+        tmp.close()
+        os.unlink(tmp.name)
+        os.rename(sys.executable, tmp.name)
+        ctypes.windll.kernel32.MoveFileExW(tmp.name, None, MOVEFILE_DELAY_UNTIL_REBOOT)
 
-    os.rename(os.path.join(build_dir, 'python'), interpreter_path)
+        os.rename(os.path.join(build_dir, "python.exe"), interpreter_path)
+    elif platform.system() == "Linux":
+        sysconfig_libs = []
+        sysconfig_lib_dirs = []
+        for arg in (
+            ["-lm", "-pthread", "-lutil", "-ldl"]
+            + sysconfig.get_config_var("LDFLAGS").split()
+            + sysconfig.get_config_var("CFLAGS").split()
+            + sysconfig.get_config_var("MODLIBS").split()
+            + sysconfig.get_config_var("LIBS").split()
+        ):
+            if arg.startswith("-l"):
+                if arg[2:] not in sysconfig_libs:
+                    sysconfig_libs.append(arg[2:])
+            elif arg.startswith("-L"):
+                if arg[2:] not in sysconfig_lib_dirs:
+                    sysconfig_lib_dirs.append(arg[2:])
 
-    with open(os.path.join(sysconfig.get_config_var('prefix'), 'link.json'), 'w') as f:
-        json.dump({
-            'include_dirs': include_dirs,
-            'macros': macros,
-            'libraries': link_libs,
-            'library_dirs': library_dirs
-        }, f)
+        link_libs = sysconfig_libs + link_libs
+        library_dirs = sysconfig_lib_dirs + library_dirs
+
+        compiler.compile(
+            [os.path.join(sysconfig.get_config_var("prefix"), "python.c")],
+            output_dir="/",
+            include_dirs=include_dirs,
+            macros=macros,
+        )
+
+        compiler.link_executable(
+            objects=[os.path.join(sysconfig.get_config_var("prefix"), "python.o")],
+            output_progname="python",
+            output_dir=build_dir,
+            libraries=link_libs,
+            library_dirs=library_dirs,
+            extra_preargs=sysconfig.get_config_var("LDFLAGS").split()
+            + [
+                "-flto",
+                "-fuse-linker-plugin",
+                "-ffat-lto-objects",
+                "-flto-partition=none",
+            ],
+        )
+
+        # Replace running interpreter by moving current version to a temp file, then deleting it. This
+        # is to avoid Windows locks
+        interpreter_path = os.path.realpath(sys.executable)
+        tmp = tempfile.NamedTemporaryFile(
+            delete=False, dir=os.path.dirname(sys.executable)
+        )
+        tmp.close()
+        os.unlink(tmp.name)
+        os.rename(sys.executable, tmp.name)
+        os.unlink(tmp.name)
+
+        os.rename(os.path.join(build_dir, "python"), interpreter_path)
+
+    with open(os.path.join(interpreter_prefix, "link.json"), "w") as f:
+        json.dump(
+            {
+                "include_dirs": include_dirs,
+                "macros": macros,
+                "libraries": link_libs,
+                "library_dirs": library_dirs,
+            },
+            f,
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_rebuild()
