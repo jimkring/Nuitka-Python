fddbd03a2a34e2071c43d7380c3758b9d49a56cb
Kay Hayen <kay.hayen@gmail.com>
2022-01-30 10:18:29 +0100

    Copy that script too

--- dev-pip.sh
+++ dev-pip.sh
@@ -2,6 +2,7 @@
 
 target=/opt/nuitka-python27
 
+sudo cp Lib/rebuildpython.py $target/lib/python2.7/
 sudo cp Lib/pip.py $target/lib/python2.7/
 sudo cp Lib/__np__/*.py $target/lib/python2.7/__np__
 
