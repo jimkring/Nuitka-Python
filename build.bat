54e284b1aa023cb646d8d852a072c663b00567c9
Stas Fomin <stas-fomin@yandex.ru>
2022-05-11 14:31:24 +0300

    Fix path hardcoding (#3)
    * Fix path hardcoding
    
    * PGD file exists only for PGD compilation, not exists for win32 crosscompilation
    
    * Remove hardcoding to x64 by default, adapted to build both x64 and x86, depending on option
    
    * Unfortunately, no PGO when compiled win32, configuration should be Release
    
    * Adapt to building x64 to 'output' folder and build w32 to 'output32' folder
    
    * Requested changes for https://github.com/Nuitka/Nuitka-Python/pull/3

--- build.bat
+++ build.bat
@@ -21,18 +21,23 @@ rem Build with nuget, it solves the directory structure for us.
 call .\Tools\nuget\build.bat %ARCH_OPT% %PGO_OPT% %REBUILD_OPT%
 
 rem Install with nuget into a build folder
-.\externals\windows-installer\nuget\nuget.exe install python -Source %~dp0\PCbuild\amd64 -OutputDirectory nuget-result
+if "%ARCH_OPT%" EQU "-x64" (.\externals\windows-installer\nuget\nuget.exe install python -Source %~dp0\PCbuild\amd64 -OutputDirectory nuget-result) 
+else (.\externals\windows-installer\nuget\nuget.exe install pythonx86 -Source %~dp0\PCbuild\win32 -OutputDirectory nuget-result)
 
 rem Move the standalone build result to "output". TODO: Version number could be queried here
 rem from the Python binary built, or much rather we do not use one in the nuget build at all.
-xcopy /i /q /s /y nuget-result\python.3.9.5\tools output
-
-md output\dependency_libs\openssl\lib
+if "%ARCH_OPT%" EQU "-x64" (
+    xcopy /i /q /s /y nuget-result\python.3.9.5\tools output
+    md output\dependency_libs\openssl\lib
+    ) else (
+    xcopy /i /q /s /y nuget-result\pythonx86.3.9.5\tools output32
+    md output32\dependency_libs\openssl\lib
+    )
 
 for /d %%d in (externals\openssl*) do (
-if "%ARCH_OPT%" EQU "-x64" ( xcopy /i /q /s /y %%d\amd64\*.lib output\dependency_libs\openssl\lib && xcopy /i /q /s /y %%d\amd64\include output\dependency_libs\openssl\include ) else ( xcopy /i /q /s /y %%d\win32\*.lib output\dependency_libs\openssl\lib && xcopy /i /q /s /y %%d\win32\include output\dependency_libs\openssl\include )
+if "%ARCH_OPT%" EQU "-x64" ( xcopy /i /q /s /y %%d\amd64\*.lib output\dependency_libs\openssl\lib && xcopy /i /q /s /y %%d\amd64\include output\dependency_libs\openssl\include ) else ( xcopy /i /q /s /y %%d\win32\*.lib output32\dependency_libs\openssl\lib && xcopy /i /q /s /y %%d\win32\include output32\dependency_libs\openssl\include )
 )
 
-echo "Ok, Nuitka Python now lives in output folder"
+echo "Ok, Nuitka Python now lives in output[32] folder"
 
 endlocal
