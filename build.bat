b5f6860b32860961aa40966362da8b1a3ce1a83a
Stas Fomin <stas-fomin@yandex.ru>
2022-05-25 07:54:09 +0300

    want to fix x64 build right now (#5)
    * Fix path hardcoding
    
    * PGD file exists only for PGD compilation, not exists for win32 crosscompilation
    
    * Remove hardcoding to x64 by default, adapted to build both x64 and x86, depending on option
    
    * Unfortunately, no PGO when compiled win32, configuration should be Release
    
    * Adapt to building x64 to 'output' folder and build w32 to 'output32' folder
    
    * Requested changes for https://github.com/Nuitka/Nuitka-Python/pull/3
    
    * improve readability and agility of x64 and x86 compilation. X64 python compiled and can be recompiled
    
    * shutil.move works even if in TEMP dir on different disk - catch an error
    
    * Dont add PGO params on Win32
    
    * Lets ignore all nuget results (different archs can exists in parallel)
    
    * We need lzma.dll (exists only in Win32) in root python folder
    
    * We have to explicilty specify platform Win32
    
    * Remove commented code, moving conf vars to top
    
    * win32-python will be placed to output32, need ignore
    
    * fix a typo, return back as it be before my patches

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 setlocal enableextensions
+@echo on
 
 rem Go home.
 cd %~dp0
@@ -13,31 +14,40 @@ if "%~1" EQU "-x64" (set ARCH_OPT=-x64) && shift && goto CheckOpts
 if "%~1" EQU "--pgo" (set PGO_OPT=--pgo) && shift && goto CheckOpts
 if "%~1" EQU "-r" (set REBUILD_OPT=-r) && shift && goto CheckOpts
 
+set NUGET_PYTHON_PACKAGE_NAME=python
+set ARCH_NAME=amd64
+if "%ARCH_OPT%" EQU "-x86" (
+    set NUGET_PYTHON_PACKAGE_NAME=pythonx86
+    set ARCH_NAME=win32
+)
+
 
 rem Remove old output
-del /S /Q output nuget-result >nul
+del /S /Q output nuget-result-%NUGET_PYTHON_PACKAGE_NAME% >nul
 
 rem Build with nuget, it solves the directory structure for us.
 call .\Tools\nuget\build.bat %ARCH_OPT% %PGO_OPT% %REBUILD_OPT%
 
 rem Install with nuget into a build folder
-if "%ARCH_OPT%" EQU "-x64" (.\externals\windows-installer\nuget\nuget.exe install python -Source %~dp0\PCbuild\amd64 -OutputDirectory nuget-result) 
-else (.\externals\windows-installer\nuget\nuget.exe install pythonx86 -Source %~dp0\PCbuild\win32 -OutputDirectory nuget-result)
+.\externals\windows-installer\nuget\nuget.exe install %NUGET_PYTHON_PACKAGE_NAME% -Source %~dp0\PCbuild\%ARCH_NAME% -OutputDirectory nuget-result-%NUGET_PYTHON_PACKAGE_NAME%
 
 rem Move the standalone build result to "output". TODO: Version number could be queried here
 rem from the Python binary built, or much rather we do not use one in the nuget build at all.
-if "%ARCH_OPT%" EQU "-x64" (
-    xcopy /i /q /s /y nuget-result\python.3.9.5\tools output
-    md output\dependency_libs\openssl\lib
-    ) else (
-    xcopy /i /q /s /y nuget-result\pythonx86.3.9.5\tools output32
-    md output32\dependency_libs\openssl\lib
-    )
 
-for /d %%d in (externals\openssl*) do (
-if "%ARCH_OPT%" EQU "-x64" ( xcopy /i /q /s /y %%d\amd64\*.lib output\dependency_libs\openssl\lib && xcopy /i /q /s /y %%d\amd64\include output\dependency_libs\openssl\include ) else ( xcopy /i /q /s /y %%d\win32\*.lib output32\dependency_libs\openssl\lib && xcopy /i /q /s /y %%d\win32\include output32\dependency_libs\openssl\include )
+set OUTPUT_DIR=output
+set SRC_TOOLS_DIR=nuget-result-%NUGET_PYTHON_PACKAGE_NAME%\%NUGET_PYTHON_PACKAGE_NAME%.3.9.5\tools
+set SRC_LIB_DIR=%%d\amd64
+if "%ARCH_OPT%" EQU "-x86" (
+    set OUTPUT_DIR=output32
+    set SRC_LIB_DIR=%%d\win32
 )
 
-echo "Ok, Nuitka Python now lives in output[32] folder"
+xcopy /i /q /s /y %SRC_TOOLS_DIR% %OUTPUT_DIR%
+
+for /d %%d in (externals\openssl*) do (
+   xcopy /i /q /s /y %SRC_LIB_DIR%\*.lib %OUTPUT_DIR%\dependency_libs\openssl\lib && xcopy /i /q /s /y %SRC_LIB_DIR%\include %OUTPUT_DIR%\dependency_libs\openssl\include 
+)
+
+echo "Ok, Nuitka Python now lives in %OUTPUT_DIR% folder"
 
 endlocal
