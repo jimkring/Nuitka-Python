394bf5be08ff00b26467986ea252108351172035
Maxwell Dreytser <admin@mdtech.us>
2023-12-27 22:16:40 -0500

    Prevent error on unsupported distros.

--- build.sh
+++ build.sh
@@ -3,17 +3,20 @@
 
 # Install Debian dependencies.
 # TODO: Support Fedora/CentOS/etc. as well.
-pkgs='build-essential checkinstall tk-dev libc6-dev'
-install=false
-for pkg in $pkgs; do
-  status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
-  if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
-    install=true
-    break
+if command -v apt &> /dev/null
+then
+  pkgs='build-essential libc6-dev'
+  install=false
+  for pkg in $pkgs; do
+    status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
+    if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
+      install=true
+      break
+    fi
+  done
+  if "$install"; then
+    sudo apt install $pkgs
   fi
-done
-if "$install"; then
-  sudo apt install $pkgs
 fi
 
 set -e
